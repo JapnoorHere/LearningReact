be18ed74c15d6d75c9ca635983640b43ce216064
Japnoor Singh <sjapnoor11@gmail.com>
2024-05-31 21:40:33 +0530

    Learnt about use Effect

---
+++ 4/src/UseEffect.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+
+const UseEffect = () => {
+    const [clicked,setClicked] = useState("");
+    const [clicked1,setClicked1] = useState("");
+    
+    useEffect(()=>{
+        alert("We r in use state");
+    },[clicked])
+  return (
+    <div>
+        <button onClick={()=>{setClicked("Subscribe")}}>Subscribe</button>    
+        <br/>
+        <hr/>
+        <button onClick={()=>{setClicked("To")}}>To</button>    
+        <br/>
+        <hr/>
+        <button onClick={()=>{setClicked1("Droid Bytes")}}>Droid Bytes</button>    
+        <h1>{clicked}</h1>
+        <h1>{clicked1}</h1>
+    </div>
+  )
+}
+
+export default UseEffect
